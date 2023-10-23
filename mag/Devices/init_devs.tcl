32add58490dbdafcd464c54e336ebc1e6904c6a1
JakobRat <74247834+JakobRat@users.noreply.github.com>
2023-10-23 15:53:47 +0200

    Added semi-automatic generated layout.

---
+++ mag/Devices/init_devs.tcl
@@ -0,0 +1,13 @@
+load XM1_xbuf3_xringosc
+::sky130::sky130_fd_pr__nfet_01v8_draw {w 10.0 l 0.5 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
+select cell XM1_xbuf3_xringosc
+writeall force
+load XM2A_xbuf3_xringosc
+::sky130::sky130_fd_pr__pfet_01v8_draw {w 10.0 l 0.5 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
+select cell XM2A_xbuf3_xringosc
+writeall force
+load XM2B_xbuf3_xringosc
+::sky130::sky130_fd_pr__pfet_01v8_draw {w 10.0 l 0.5 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
+select cell XM2B_xbuf3_xringosc
+writeall force
+quit -noprompt
